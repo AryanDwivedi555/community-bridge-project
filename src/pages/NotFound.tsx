fc29fc313758814ae4f69e3a62542fa0fc881d46
Sarthak-Nayak <nsarthak458@gmail.com>
2026-04-01 09:21:55 +0530

    Fix useEffect indentation in NotFound component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const NotFound = () => {
   const location = useLocation();
 
-  useEffect(() => {
+  useEffect(() => { 
     console.error("404 Error: User attempted to access non-existent route:", location.pathname);
   }, [location.pathname]);
 
